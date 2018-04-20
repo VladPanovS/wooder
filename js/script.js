eeddfb31c4b225bbd3340f07b8cf6f4752d44e50
VladPanovS <34751539+VladPanovS@users.noreply.github.com>
2018-04-20 16:51:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+  $("#navmenu"), $("#menu").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  })
   $('.header-slider').slick({
     arrows: false,
     dots: true,
     fade: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
   });
 });
